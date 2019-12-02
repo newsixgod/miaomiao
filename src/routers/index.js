3fca25ea0d1d0596cea7eac25bfa4b5776d2b295
newsixgod <1101128058@qq.com>
2019-12-02 20:25:24 +0800

    add createComponents

---
+++ src/routers/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes:[
+    movieRouter,
+    mineRouter,
+    cinemaRouter,
+    {
+      path : '/*',
+      redirect : '/movie'
+    }
+  ]
+})
